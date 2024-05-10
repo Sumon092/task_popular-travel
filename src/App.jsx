bbb292c70956e546b22f032667ce2c46b21faf0e
Sumon <m.sumon@outlook.com>
2024-05-10 20:13:42 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-start items-center h-[100vh]">
+      <h1 className="text-2xl text-green-600 font-bold">Hello Popular Travel</h1>
+    </div>
+  );
+};
+
+export default App;
